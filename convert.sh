835fd291db4862195280d91efe1cdd5b314457dc
xie <709955748@qq.com>
2023-01-13 10:05:54 +0800

    add remove

--- convert.sh
+++ convert.sh
@@ -2,3 +2,4 @@ rm -rf ./Music
 adb pull /storage/emulated/0/netease/cloudmusic/Music
 node ./src/ncm-api.js
 adb push ./Music/*.mp3 /sdcard/Download/musics/
+adb shell rm -rf /storage/emulated/0/netease/cloudmusic/Music/*
